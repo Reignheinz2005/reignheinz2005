5aef3d7127a8a0c1d2de15fab6cb2de30790e192
Reignheinz2005 <152366043+Reignheinz2005@users.noreply.github.com>
2023-12-01 14:52:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,9 @@ st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 # ---- HEADER SECTION ----
 with st.container():
-st.subheader("Hi, I am Reinzler")
+st.subheader("Hi I am Reign Heinz")
 st.title("A Students from SNSU")
 st.write("I am passionate to learn from this amazing school")
-
 
     
         

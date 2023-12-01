1206a8b9677d7893b0ce6d0d7f77967613493f3f
Reignheinz2005 <152366043+Reignheinz2005@users.noreply.github.com>
2023-12-01 15:02:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,23 @@ with st.container():
  st.title("A Students from SNSU")
  st.write("I am passionate to learn from this amazing school")
 
-        
-  
-    
+# ---- WHAT I DO ----
+with st.container():
+  st.write("---")
+  left_column, right_column = st.columns(2)
+  with left_column:
+    st.header("What I do")
+    st.write("##")
+    st.write(
+        """
+        On my journey of being a student i will show you the beautiful place to learn from this school for people that want to learn from this university:
+        REQUIREMENTS TO ENROLL:
+        - 2x2 pic
+        - birth certificate
+        - good moral and report card
+
+        If you have any further question just message me in my Fb acc: Reign Heinz Jeisler Egnalig or my Github Acc.
+        """
+    )
+    st.write("[My Github Account >](https://github.com/Reignheinz2005/reignheinz2005/blob/main/streamlit_app.py")
+

cfb19790f3aab432606a41a3a8471ccc339b6f4e
Reignheinz2005 <152366043+Reignheinz2005@users.noreply.github.com>
2023-12-01 14:48:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 # ---- HEADER SECTION ----
 with st.container():
-st.subheader("Hi, I am Reign Heinz Jeisler Egnalig")
+st.subheader("Hi, I am Reinzler")
 st.title("A Students from SNSU")
 st.write("I am passionate to learn from this amazing school")
 
@@ -24,7 +24,7 @@ with st.container():
         - birth certificate
         - good moral and report card
 
-        If you have any further question just message me in my Fb acc: Reign Heinz Jeisler Egnalig
+        If you have any further question just message me in my Github Acc:
         """
     
     

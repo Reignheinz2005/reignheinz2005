324a9e40ca6d7e186de3589cde4cdad2401fd951
Reignheinz2005 <152366043+Reignheinz2005@users.noreply.github.com>
2023-12-01 14:36:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,30 @@ import streamlit as st
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 # ---- HEADER SECTION ----
-st.subheader("Hi, I am Reinzler")
+with st.container():
+st.subheader("Hi, I am Reign Heinz Jeisler Egnalig")
 st.title("A Students from SNSU")
 st.write("I am passionate to learn from this amazing school")
+
+# ---- WHAT I DO ----
+with st.container():
+  st.write("---")
+  left_column, right_column = st.columns(2)
+  with left_column:
+    st.header("What I do")
+    st.write("##")
+    st.write(
+        """
+        On my journey of being a student i will show you the beautiful place to learn from this school for people that want to learn from this university:
+        REQUIREMENTS TO ENROLL:
+        - 2x2 pic
+        - birth certificate
+        - good moral and report card
+
+        If you have any further question just message me in my Fb acc: Reign Heinz Jeisler Egnalig
+        """
+    )
+    
+        
+  
+    

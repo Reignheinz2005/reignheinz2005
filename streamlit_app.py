a34889ad16f2e1048be015b628bde89894c26b73
Reignheinz2005 <152366043+Reignheinz2005@users.noreply.github.com>
2023-12-05 12:14:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,6 @@ st.set_page_config(
 
 st.title("Main Page")
 st.sidebar.success("Select a page above.")
-# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
-
-# ---- HEADER SECTION ----
 with st.container():
  st.subheader("Hi I am Reign Heinz")
  st.title("A Students from SNSU")

64bfca7115dea3ed4fe8f7f4b28971d7ba025356
Reignheinz2005 <152366043+Reignheinz2005@users.noreply.github.com>
2023-12-08 14:58:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,5 @@ st.set_page_config(
 st.title("Main Page")
 st.sidebar.success("Select a page above.")
 
-import streamlit as st
 
-# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
-
-# ---- HEADER SECTION ----
-with st.container():
- st.subheader("Hi I am Reign Heinz")
- st.title("A Students from SNSU")
- st.write("I am passionate to learn from this amazing school")
-
-# ---- WHAT I DO ----
-with st.container():
-  st.write("---")
-  left_column, right_column = st.columns(2)
-  with left_column:
-    st.header("What I do")
-    st.write("##")
          

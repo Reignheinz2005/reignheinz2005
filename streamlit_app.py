07b73aa8a0c969b654b9d19dcccfdbf779208170
Reignheinz2005 <152366043+Reignheinz2005@users.noreply.github.com>
2023-12-08 13:55:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ with st.container():
     st.write("##")
     st.write()
         
-  On his youtube channel we will learn to make website using python, check his youtube tutorial to people who:
+  on his youtube channel we will learn to make website using python, check his youtube tutorial to people who:
         - are willing to learn python 
         - are struggling to their projects
         - are lack of knowledge about python 
